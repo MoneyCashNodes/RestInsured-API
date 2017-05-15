809a80e04b94a25f9d75735ca049cbf18ba7e6f6
Benjamin <ayzenbergb@gmail.com>
2017-05-15 15:41:18 -0700

    return the router

--- routes/doctor-rt.js
+++ routes/doctor-rt.js
@@ -1,30 +1,29 @@
 'use strict';
 
 const debug = require('debug')('restInsured: doctor-rt');
-const bearerAuth = require('../middleware/bearer-auth');
-const express = require('express');
-const app = express();
+// const bearerAuth = require('../middleware/bearer-auth');
+// const express = require('express');
+// const app = express();
 
+// /ext/doctors?location=${req.query.location},${req.query.range}&insurance_uid=${req.query.insurance}&user_key${process.env.user_key}
 
-module.exports = function (router) {
+module.exports = function(router) {
+  router.get('/doctors', (req, res) => {
+    debug('GET /doctors');
 
-  router.get('/doctors', (req,res) => {
-    debug('GET /resource_url');
-
-
-    let reqUrl = `https://api.betterdoctor.com/${new Date.parse()}/doctors?location=${req.query.location},${req.query.range}&insurance_uid=${req.query.insurance}&user_key${process.env.user_key}`;
-    app.use(reqUrl, data => {
-      console.log(data);
+    // let reqUrl = `https://api.betterdoctor.com/${new Date.parse()}/doctors?location=${req.query.location},${req.query.range}&insurance_uid=${req.query.insurance}&user_key${process.env.user_key}`;
+    // app.use(reqUrl, data => {
+      // console.log(data);
       // let body = {
       //   property: data.property,
       // };
 
       // res.json(body);
-    });
-
-    console.log('this is req ' + req);
-    console.log('this is res ' +res);
+    // });
+    //
+    // console.log('this is req ' + req);
+    // console.log('this is res ' +res);
 
   });
-
+  return router;
 };
