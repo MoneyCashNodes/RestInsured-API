824a1d05342e594e46187e1cabfd7753eb5ac05e
Kayla Asay <kayla-asay@Ubuntu16.04-01>
2017-05-15 12:57:48 -0700

    user route

--- routes/user-rt.js
+++ routes/user-rt.js
@@ -1,1 +1,34 @@
 'use strict'
+
+const debug = require('debug')('');
+const User = require('../model/user');
+const basicAuth = require('../middleware/basic-auth');
+
+module.exports = function(router) {
+  router.post('/signup', (req, res) => {
+    debug('POST /signup')
+
+    let tempPassword = req.body.password;
+    req.body.password = null;
+    delete req.body.password;
+
+    let newUser = new User(req.body);
+
+    return newUser.generatePasswordHash(tempPassword)
+    .then(user => user.save())
+    .then(user => user.generateToken())
+    .then(token => res.json(token))
+    .catch(err => res.status(err.status).send(err))
+  })
+
+  router.get('/signin', basicAuth, (req, res) => {
+    debug('GET /signin')
+
+    return User.findOne({fullname: req.auth.fillname})
+    .then(user => user.comparePasswordHash(req.auth.fullname))
+    .then(user => user.generateToken())
+    .then(token => res.json(token))
+    .catch(err => res.status(err.status).send(err))
+  })
+  return router;
+}
