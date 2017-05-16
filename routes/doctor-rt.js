13ce5682be1f088425d5329d3d800da1397975b7
Benjamin <ayzenbergb@gmail.com>
2017-05-16 10:34:57 -0700

    Functional external API call

--- routes/doctor-rt.js
+++ routes/doctor-rt.js
@@ -2,8 +2,7 @@
 
 const debug = require('debug')('restInsured: doctor-rt');
 // const bearerAuth = require('../middleware/bearer-auth');
-const express = require('express');
-const app = express();
+const rp = require('request-promise');
 
 // /ext/doctors?location=${req.query.location},${req.query.range}&insurance_uid=${req.query.insurance}&user_key${process.env.user_key}
 
@@ -11,16 +10,12 @@ module.exports = function(router) {
   router.get('/doctors', (req, res) => {
     debug('GET /doctors');
 
-    let reqUrl = `'https://api.betterdoctor.com/2016-03-10/doctors?location=${req.query.location},${req.query.range}&insurance_uid=${req.query.insurance}&user_key=${process.env.user_key}&limit=10'`;
+    let reqUrl = `https://api.betterdoctor.com/2016-03-01/doctors?insurance_uid=${req.query.insurance}&location=${req.query.lat}%2C${req.query.lon}%2C${req.query.range}&limit=10&user_key=${process.env.user_key}`;
+
     console.log(reqUrl);
-    app.get(reqUrl, data => {
-      console.log(data);
-      // let body = {
-      //   property: data.property,
-      // };
-      //
-      // res.json(body);
-    });
+    rp(reqUrl)
+    .then(data => res.json(JSON.parse(data)))
+    .catch(err => err);
   });
   return router;
 };
