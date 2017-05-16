9f5fca5d29280de35c6a66d4556a69d1cb3907d0
Enrique Rico <enriquesantosrico@gmail.com>
2017-05-16 15:07:42 -0700

    still working on range but doctor/practice object works

--- controllers/doctor-ctrl.js
+++ controllers/doctor-ctrl.js
@@ -6,11 +6,30 @@ module.exports = exports = {};
 
 exports.reduce = function(data) {
   debug('#REDUCE doctor-ctrl');
+  console.dir(data);
+  let doctorHouse = [];
+
   data.data.forEach(practice => {
-    practice.practices.forEach((location, i) => {
-      console.log(i);
-      console.log(location.name);
-    });
+
+    let medicalPractice = {
+      name: practice.practices[0].name,
+      lat: practice.practices[0].visit_address.lat,
+      lon: practice.practices[0].visit_address.lon,
+      street: practice.practices[0].visit_address.street,
+      zip: practice.practices[0].visit_address.zip,
+      city:practice.practices[0].visit_address.city,
+      state: practice.practices[0].visit_address.state,
+      phone: practice.practices[0].phones[0].number,
+      doctor: {
+        doctorfirstname: practice.profile.first_name,
+        doctorlastname: practice.profile.last_name,
+        specialty: practice.specialties[0].name,
+      },
+    };
+
+    doctorHouse.push(medicalPractice);
+
   });
-  return data;
+
+  return doctorHouse;
 };
--- routes/doctor-rt.js
+++ routes/doctor-rt.js
@@ -10,7 +10,7 @@ module.exports = function(router) {
   router.get('/doctors', (req, res) => {
     debug('GET /doctors');
 
-    let reqUrl = `https://api.betterdoctor.com/2016-03-01/doctors?insurance_uid=${req.query.insurance}&location=${req.query.lat}%2C${req.query.lon}%2C${req.query.range}&limit=10&user_key=${process.env.user_key}`;
+    let reqUrl = `https://api.betterdoctor.com/2016-03-01/doctors?insurance_uid=${req.query.insurance}&location=${req.query.lat}%2C${req.query.lon}%2C${req.query.range}&limit=5&user_key=${process.env.user_key}`;
 
     rp(reqUrl)
     .then(data => doctorCtrl.reduce(JSON.parse(data)))
