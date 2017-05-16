d0b86ac23624dc3b58cea194c53b4b64a6547b1a
Benjamin <ayzenbergb@gmail.com>
2017-05-16 11:02:44 -0700

    usable data

--- routes/doctor-rt.js
+++ routes/doctor-rt.js
@@ -12,7 +12,6 @@ module.exports = function(router) {
 
     let reqUrl = `https://api.betterdoctor.com/2016-03-01/doctors?insurance_uid=${req.query.insurance}&location=${req.query.lat}%2C${req.query.lon}%2C${req.query.range}&limit=10&user_key=${process.env.user_key}`;
 
-    console.log(reqUrl);
     rp(reqUrl)
     .then(data => res.json(JSON.parse(data)))
     .catch(err => err);
