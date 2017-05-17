18e034e2392b204fbc69a9cc937fd7245f74db2b
Benjamin <ayzenbergb@gmail.com>
2017-05-17 11:03:07 -0700

    success tests passing

--- test/doctor-rt-tst.js
+++ test/doctor-rt-tst.js
@@ -6,7 +6,7 @@ const request = require('superagent');
 const server = require('../server.js');
 
 
-describe.only('BetterDoc route', function(){
+describe.only('BetterDoc route', function() {
   let app;
   before(done => {
     app = server.listen(8000);
@@ -18,52 +18,62 @@ describe.only('BetterDoc route', function(){
     done();
   });
 
-  describe('GET request', function(){
-
-    it('should have a doctor property with valid name and phone number', done => {
+  describe('GET request /ext/doctors', function() {
+    before(done => {
       request.get('localhost:8000/ext/doctors?lat=47.606&lon=-122.332&range=10&insurance=regenceblueshieldofwashinton-regencewapreferredprovidernetwork')
-      // .query(
-      //   {
-      //     user_key: process.env.user_key,
-      //     insurance_uid: insurance,
-      //     user_location: sampleloc,
-      //     location: sampleloc,range,
-      //   })
         // .set({
         //   Authorization: `Bearer ${this.temptoken}`,
         // })
-        .end((err,res) =>{
+        .end((err,res) => {
           if(err) return done(err);
-          console.log(res.body);
-          expect(res.body.doctor.name).to.be.a('String');
-          expect(res.body.doctor.phonenumber).to.be.a('String');
+          this.res = res;
           done();
         });
     });
 
-    // it('should have a practice property with valid name, specialty, phone number,insurance provider, location', done => {
-    //   app.get('/doctors')
-    //   .query(
-    //     {
-    //       user_key: process.env.user_key,
-    //       insurance_uid: insurance,
-    //       user_location: sampleloc,
-    //       location: sampleloc,range,
-    //     })
-    //     .set({
-    //       Authorization: `Bearer ${this.temptoken}`,
-    //     })
-    //     .end((err,res) =>{
-    //       if(err) return done(err);
-    //       expect(res.body.practice.name).to.be.a('String');
-    //       expect(res.body.practice.phonenumber).to.be.a('String');
-    //       expect(res.body.practice.insuranceproviderid).to.be.a('String');
-    //       expect(res.body.practice.specialty).to.be.a('String');
-    //       expect(res.body.practice.location).to.be.a('String');
-    //       done();
-    //     });
-    // });
+    it('should have a doctor property with valid name and specialty', () => {
+      expect(this.res.body[0].doctor.doctorfirstname).to.be.a('String');
+      expect(this.res.body[0].doctor.doctorlastname).to.be.a('String');
+      expect(this.res.body[0].doctor.specialty).to.be.a('String');
+    });
 
+    it('should have a doctor property with valid name and phone number', () => {
+      expect(this.res.body[0].name).to.be.a('String');
+      expect(this.res.body[0].lat).to.be.a('Number');
+      expect(this.res.body[0].lon).to.be.a('Number');
+      expect(this.res.body[0].street).to.be.a('String');
+      expect(this.res.body[0].zip).to.be.a('String');
+      expect(this.res.body[0].city).to.be.a('String');
+      expect(this.res.body[0].state).to.be.a('String');
+      expect(this.res.body[0].phone).to.be.a('String');
+    });
+
+    it('should be an array of 5 objects', () => {
+      expect(this.res.body).to.be.an('Array');
+      expect(this.res.body).length(5);
+      expect(this.res.body[0]).to.be.an('Object');
+      expect(this.res.body[0].doctor).to.be.an('Object');
+    });
+
+    it('should have a status of 200 on a proper request', () => {
+      expect(this.res.status).to.equal(200);
+    });
   });
 
+  // describe('GET request no params', function() {
+  //   before(done => {
+  //     request.get('localhost:8000/ext/doctors')
+  //       // .set({
+  //       //   Authorization: `Bearer ${this.temptoken}`,
+  //       // })
+  //       .end((err,res) => {
+  //         this.res = res;
+  //         this.err = err;
+  //         done();
+  //       });
+  //   });
+  //   it('should return a 400 error on a bad request', () => {
+  //     expect(this.res.status).to.equal(400);
+  //   });
+  // });
 });
